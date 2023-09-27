4a5f550fefa3c59bf93f5f8d393b03982c86dc5a
sachin <84963232+sachin631@users.noreply.github.com>
2023-09-27 22:36:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,55 +13,10 @@ const userRouter = require("./router/userRouter");
 const orderRouter = require("./router/orderRouter");
 const PORT = process.env.PORT || 27017;
 
-// app.use(cors())
- app.use(
-    cors({
-      origin: "https://main--precious-tarsier-661a39.netlify.app",
-      credentials: true,
-    })
-  );
+
+ 
 try {
-  const corsOptions = {
-  origin: 'https://65070a2ea65d6c0007738a7a--precious-tarsier-661a39.netlify.app/',
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-  
-  // app.use(
-  //   cors({
-  //     origin: "http://localhost:3000",
-  //     credentials: true,
-  //   })
-  // );
-  // app.use(
-  //   cors({
-  //     origin: "https://main--precious-tarsier-661a39.netlify.app",
-  //     credentials: true,
-  //   })
-  // );
-
-  // Allow requests only from specific origins
-// const allowedOrigins = [
-//   "http://localhost:3000", // Add your local development origin if needed
-//   "https://main--precious-tarsier-661a39.netlify.app",
-// ];
-
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // Check if the request origin is in the list of allowed origins
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
-
-
+ app.use(cors())
   app.use(express.json({ limit: "10mb" }));
 
   app.use("/uploads", express.static("./uploads"));
@@ -72,10 +27,6 @@ app.use(cors(corsOptions));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(fileUpload());
 
-  // app.use(fileUpload({
-  //   useTempFiles:true
-  // }))  //this code show errror in multer
-  // app.use(bodyParser.urlencoded({extended:true})); // from express.json() or bodyParser use any one to deal with json file
 
   //always take router in last
   app.use(productRouter);
